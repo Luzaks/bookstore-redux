6e6bae1c02209b40b69c9bf8e67cb9a870947f68
Luzaks <kanemekanik@gmail.com>
2020-07-09 11:29:58 -0500

    Style: Add intermediate styles for nav.

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { FaUser } from 'react-icons/fa';
 import Book from '../components/Book';
 import { removeCreator, filterCreator } from '../actions';
 import CategoryFilter from '../components/CategoryFilter';
-import { FaUser } from 'react-icons/fa'
 
 const filterHelper = event => event.target.value;
 
@@ -30,20 +30,22 @@ const BooksList = ({
     <nav className="bookstore-nav">
       <div className="nav-left">
         <h1 className="bookstore-logo">Bookstore CMS</h1>
-        <h3 className="books-link">Books</h3>
+        <h3 className="books-link">BOOKS</h3>
         <CategoryFilter handleFilterChange={handleFilterChange} />
       </div>
       <div className="nav-right user-icon">
         <FaUser />
       </div>
     </nav>
-    <table className="books-table">
-      {
-        renderHelper(filter, books).map(book => (
-          <Book key={book.id} book={book} handleRemoveBook={handleRemoveBook} />
-        ))
-      }
-    </table>
+    <div className="table-container">
+      <table className="books-table">
+        {
+          renderHelper(filter, books).map(book => (
+            <Book key={book.id} book={book} handleRemoveBook={handleRemoveBook} />
+          ))
+        }
+      </table>
+    </div>
   </div>
 );
 
---
+++ ss
@@ -0,0 +1,6 @@
+  Milestone_1[m
+  Milestone_2[m
+  Milestone_3[m
+  Milestone_4[m
+* [32mMilestone_5[m
+  development[m
